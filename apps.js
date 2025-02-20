d65c4740ca66afd1e3c79a982706d8e8ded59173
dambers <demetris.ambers@outlook.com>
2025-02-19 18:27:18 -0600

    update

---
+++ apps.js
@@ -0,0 +1,62 @@
+var life = 200 ;
+
+life = life - 100;
+
+life = life / 50;
+
+var name = "Morning";
+
+var checkout = false;
+
+var box = null;
+
+console.log(box);
+
+
+let x = 3;
+
+function2();
+
+function function1(){
+    console.log(x);
+}
+
+function function2(){
+    console.log(x);
+}
+
+
+
+let y = 3;
+
+function2();
+
+function function1(){
+    let x = 1;
+    console.log(x);
+}
+
+function function2(){
+    let x = 2;
+    console.log(x);
+}
+
+
+
+
+
+const person1 = {
+    firstname: "Captain",
+    lastname: "Picard",
+    age: 50,
+    isEmployed: true,
+}
+
+console.log(person1.firstname);
+console.log(person1.lastname);
+console.log(person1.age);
+console.log(person1.isEmployed);
+
+
+
+ 
